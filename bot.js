92700ed26f2116e3f3ebb43cc75248bb563f420b
Michael Santo Domingo <msd2802@gmail.com>
2017-04-27 12:06:29 +0000

    Set up api and security keys

--- bot.js
+++ bot.js
@@ -2,10 +2,5 @@ console.log('The bot is starting')
 
 var Twit = require('twit');
 
-var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
+var config = require('./config');
+var T = new Twit(config);
---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  consumer_key:         'xMPic1TTpyUTDQcdpuM8Jt4zC',
+  consumer_secret:      'VnjdU62NnmhFq8bZdzAs8AVNkV02bsnj2hiINhgigiubMk43DD',
+  access_token:         '857176980734623744-xEgwxPIm8IMFwKzzLiAy6SVS9cYAVEq',
+  access_token_secret:  'Ap6yK47Dd9nG7DTo9rJNQtCSOn884ITbBL8HTPFqB65vY',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.  
+}
