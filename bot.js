7da7124c49331a960ac2a06565811a367d496086
Michael Santo Domingo <msd2802@gmail.com>
2017-05-10 10:47:20 +0000

    Added get request functions and variables

--- bot.js
+++ bot.js
@@ -4,3 +4,13 @@ var Twit = require('twit');
 
 var config = require('./config');
 var T = new Twit(config);
+
+var params = {
+  q: 'unicat', count: 100
+}
+
+T.get('search/tweets', params, gotData);
+
+function(err, data, response) {
+  console.log(data);
+};
